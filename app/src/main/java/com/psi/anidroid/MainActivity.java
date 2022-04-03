e88c09f1061af926a55b8e1b484ced5be01c47f9
rbento01 <rodrigobento01>
2022-04-03 21:16:58 +0100

    Primeiro Commit

--- app/src/main/java/com/psi/anidroid/MainActivity.java
+++ app/src/main/java/com/psi/anidroid/MainActivity.java
@@ -33,13 +33,8 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//asdasdasdasa
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 }

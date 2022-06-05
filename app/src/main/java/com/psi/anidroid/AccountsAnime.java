31a41f005cf2f7687df8773b405d596cf7577df2
rbento01 <aluno21890@ipt.pt>
2022-06-05 17:44:15 +0100

    Resolução de erros

--- app/src/main/java/com/psi/anidroid/AccountsAnime.java
+++ app/src/main/java/com/psi/anidroid/AccountsAnime.java
@@ -29,7 +29,6 @@ public class AccountsAnime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         storeDatainArrays(database);
-        recyclerView = findViewById(R.id.)
         myAccountAdapter = new AccountsMyAdapter(AccountsAnime.this, id_User,username,email,roles);
         recyclerView.setAdapter(myAccountAdapter);
         recyclerView.setLayoutManager((new LinearLayoutManager((AccountsAnime.this))));
